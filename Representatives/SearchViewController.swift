a9629f1a02ead89fed9bbdb114fdb1a1886ecbcf
Zach Steed <zsteed21@gmail.com>
2015-10-29 15:06:05 -0600

    beginnings

--- Representatives/SearchViewController.swift
+++ Representatives/SearchViewController.swift
@@ -8,9 +8,50 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+
+
+class SearchViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        statePicker.dataSource = self
+        
+        statePicker.delegate = self
+        
+    }
+
+    //MARK: Datasource
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        
+        return 1
+        
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return self.states.count
+        
+    }
+    
+    //MARK: Delegates
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return self.states[row]
+        
+    }
     
     //MARK: - Properties
+    
     let states = ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
     
+   
+    
 }
+
+
+
