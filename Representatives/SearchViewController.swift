5c50765af6d7c44baa39079428aa6693ff90b5d8
Taylor Mott <taylor@mottapplications.com>
2015-10-29 11:58:40 -0600

    Initial Commit
    Signed-off-by: Taylor Mott <taylor@mottapplications.com>

---
+++ Representatives/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  Representatives
+//
+//  Created by Taylor Mott on 10/28/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    //MARK: - Properties
+    let states = ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    
+}
