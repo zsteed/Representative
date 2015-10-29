851817f1952fba1e2124f7015b91f92ab346b2e1
“pkadams67” <paul.k.adams@me.com>
2015-10-29 17:37:56 -0600

    Tableview in Storyboard Setup

--- Representatives/RepresentativeController.swift
+++ Representatives/RepresentativeController.swift
@@ -9,38 +9,38 @@
 import Foundation
 
 class RepresentativeController {
-    
+
     static func searchRepsByState(state: String, callback: (representatives: [Representative]) -> Void) {
-        
+
         let url = NetworkController.searchURLByState(state)
-        
-    
-    NetworkController.dataAtURL(url) { (resultData) -> Void in
-    guard let representativeData = resultData  else {
-    print("NO DATA RETURNED")
-    callback(representatives: [])
-    return
+
+
+        NetworkController.dataAtURL(url) { (resultData) -> Void in
+            guard let representativeData = resultData  else {
+                print("NO DATA RETURNED")
+                callback(representatives: [])
+                return
+            }
+
+            do {
+                let resultsDictionary = try NSJSONSerialization.JSONObjectWithData(representativeData, options: NSJSONReadingOptions.AllowFragments) as! [String : AnyObject]
+
+                let representativeDictionaries = resultsDictionary[Representative.kResultsKey] as! [[String : String]]
+
+                var arrayOfRepresentativeModelObjects: [Representative] = []
+
+                for dictionary in representativeDictionaries {
+                    let rep = Representative(jsonDictionary: dictionary)
+                    arrayOfRepresentativeModelObjects.append(rep)
+                }
+                
+                callback(representatives: arrayOfRepresentativeModelObjects)
+                
+            } catch {
+                print("Error serializing JSON")
+                callback(representatives: [])
+                return
+            }
+        }
     }
-    
-    do {
-    let resultsDictionary = try NSJSONSerialization.JSONObjectWithData(representativeData, options: NSJSONReadingOptions.AllowFragments) as! [String : AnyObject]
-    
-    let representativeDictionaries = resultsDictionary[Representative.kResultsKey] as! [[String : String]]
-    
-    var arrayOfRepresentativeModelObjects: [Representative] = []
-    
-    for dictionary in representativeDictionaries {
-    let rep = Representative(jsonDictionary: dictionary)
-    arrayOfRepresentativeModelObjects.append(rep)
-    }
-    
-    callback(representatives: arrayOfRepresentativeModelObjects)
-    
-    } catch {
-    print("Error serializing JSON")
-    callback(representatives: [])
-    return
-    }
-    }
-}
 }
---
+++ Representatives/ResultsTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultsTableViewController.swift
+//  Representatives
+//
+//  Created by Paul Kirk Adams on 10/29/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class ResultsTableViewController: UITableViewController {
+
+    var repsArray: [Representative] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
